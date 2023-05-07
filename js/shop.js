2d5c7f1ebbfdc0d1a1c5439ba01555824eafe7ba
Daniil <daniilbelkin8@gmail.com>
2023-05-07 18:06:41 +0800

    adaptation is ready

--- js/shop.js
+++ js/shop.js
@@ -49,12 +49,97 @@ nextBtn.addEventListener('click', nextSlide);
 prevBtn.addEventListener('click', prevSlide);
 
 
-const filter = document.querySelectorAll('.filter');
-const allProducts = document.querySelector('.all-products-filter'),
-      spareParts = document.querySelector('.spare-parts-filter'),
-      accessories = document.querySelector('.accessories-filter'),
-      protection = document.querySelector('.protection-filter');
 
-function filtering(params) {
+
+const navBack = document.querySelector('.nav-background');
+const mainBtn = document.querySelector('.main-btn');
+const productsBtn = document.querySelector('.products-btn');
+const contactsBtn = document.querySelector('.contacts-btn');
+const trainingBtn = document.querySelector('.training-btn');
+
+mainBtn.addEventListener('mouseover', () => {
+    navBack.classList.add('main-back')
+    navBack.classList.remove('products-back')
+    navBack.classList.remove('contacts-back')
+    navBack.classList.remove('training-back')
+});
+productsBtn.addEventListener('mouseover', () =>{
+    navBack.classList.add('products-back')
+    navBack.classList.remove('main-back')
+    navBack.classList.remove('contacts-back')
+    navBack.classList.remove('training-back')
+});
+contactsBtn.addEventListener('mouseover', () =>{
+    navBack.classList.add('contacts-back')
+    navBack.classList.remove('products-back')
+    navBack.classList.remove('main-back')
+    navBack.classList.remove('training-back')
+});
+trainingBtn.addEventListener('mouseover', () => {
+    navBack.classList.add('training-back')
+    navBack.classList.remove('contacts-back')
+    navBack.classList.remove('main-back')
+    navBack.classList.remove('products-back')
+});
+
+const products = document.querySelectorAll('.product');
+const filtering = document.querySelector('.filtering');
+
+filtering.addEventListener('click', (filter)=> {
+    if (filter.target.tagName !== 'BUTTON') return false;
     
-}
+    let filterClass = filter.target.dataset['name'];
+    
+    products.forEach( (filter) => {
+        filter.classList.remove('hide')
+        if (!filter.classList.contains(filterClass) && filterClass !== 'all') {
+            filter.classList.add('hide');
+        }
+    });
+});
+
+
+const allProducts = document.querySelector('.all-products-filter');
+const spareParts = document.querySelector('.spare-parts-filter');
+const accessories = document.querySelector('.accessories-filter');
+const protection = document.querySelector('.protection-filter');
+
+const filters = document.querySelectorAll('.filter');
+
+
+
+filtering.addEventListener('click', (activeFilter) => {
+    if (activeFilter.target.tagName !== 'BUTTON') return false;
+    
+    filters.forEach( (activeFilter) => {
+        activeFilter.classList.add('active-filter')
+        if (!activeFilter.classList.contains(filterClass)) {
+            activeFilter.classList.remove('active-filter');
+        }
+    })
+})
+
+const menuBurger = document.querySelector('.menu-burger');
+const navigation = document.querySelector('nav');   
+const navBtns = document.querySelector('.navigation-list');
+const menuBack = document.querySelector('.assortement-container');
+const body = document.body;
+
+
+menuBurger.addEventListener('click', function() {
+    navigation.classList.toggle ('active-menu');
+    menuBurger.classList.toggle('active-menu');
+    body.classList.toggle('lock');
+    navBtns.classList.toggle('active-menu');
+    menuBack.classList.toggle('active-menu')
+});
+
+function hideMenu(){
+    body.classList.remove('lock');
+    menuBurger.classList.remove('active_menu');
+    navigation.classList.remove('active_menu');
+    navBtns.classList.remove('active-btns')
+};
+
+
+
